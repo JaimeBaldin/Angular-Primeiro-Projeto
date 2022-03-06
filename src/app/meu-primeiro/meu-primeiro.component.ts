fce07db427abf67301ba75a9f4b1c54d054ab229
“JaimeBaldin” <jaimedbaldin@gmail.com>
2022-03-05 21:50:40 -0300

    Primeiro componet e introducao ao module

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'meu-primeiro-component',
+  template: `
+    <p>Meu primeiro projeto com Angular!</p>
+  `
+})
+
+export class MeuPrimeiroComponent {}
